b12554b9b95df2dbb1f5557f67b879dc45ae57ed
Matt Spaulding <mspaulding06@gmail.com>
2013-06-12 18:38:37 -0700

    Fix example script url

--- examples/build-nightly-iso.sh
+++ examples/build-nightly-iso.sh
@@ -4,7 +4,7 @@
     --eucaversion 3.3 \
     --elrepo-repo http://mirror.symnds.com/distributions/elrepo/elrepo/el6/x86_64/ \
     --epel-repo http://dl.fedoraproject.org/pub/epel/6/x86_64/ \
-    --eucalyptus-repo http://downloads.eucalyptus.com/software/eucalyptus/nightly/3.3/centos/ \
+    --eucalyptus-repo http://downloads.eucalyptus.com/software/eucalyptus/nightly/3.3/centos/6/x86_64/ \
     --euca2ools-repo http://repos.fedorapeople.org/repos/gholms/cloud/epel-6/x86_64/ \
     --cachedir yumcache $@
 
