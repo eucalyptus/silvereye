08577c29ee1000047bd4117414371d3c66cf1129
Matt Spaulding <mspaulding06@gmail.com>
2013-09-06 10:26:08 -0700

    Fix issues with using eustore

--- anaconda-updates/6/scripts/eucalyptus-frontend-config.sh
+++ anaconda-updates/6/scripts/eucalyptus-frontend-config.sh
@@ -197,6 +197,7 @@ function get_credentials {
     cat > /root/.euca/admin.ini <<EOF
 [global]
 default-region = localhost
+eustore-url = http://emis.eucalyptus.com
 
 [region localhost]
 user = localadmin
